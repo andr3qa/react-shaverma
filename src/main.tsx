abcc88750bd2e86a64c757e0a6bd399079a16b5d
andr3qa <andr3qaa@gmail.com>
2025-09-17 20:59:45 +0400

    Installed the react-persist library. Configured the bucket to be saved in localStorage

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,17 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { persistor, store } from './store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={<div>Загрузка...</div>} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,15 @@ import sortSlice from './slices/sortSlice';
 import categoriesSlice from './slices/categoriesSlice';
 import searchSlice from './slices/searchSlice';
 import cartSlice from './slices/cartSlice';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig = {
+  key: 'cart',
+  storage,
+};
+
+const persistedCart = persistReducer(persistConfig, cartSlice);
 
 export const store = configureStore({
   reducer: {
@@ -11,9 +20,17 @@ export const store = configureStore({
     sort: sortSlice,
     categories: categoriesSlice,
     search: searchSlice,
-    cart: cartSlice,
+    cart: persistedCart,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
